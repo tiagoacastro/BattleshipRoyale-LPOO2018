024ad054ac1f94a19bae183681a8ef513ee72b2c
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-27 17:48:21 +0100

    Separation of the Server and the Client programs

---
+++ App/html/build/gwt/gen/com/google/gwt/lang/com_00046mygdx_00046game_00046GdxDefinition__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046mygdx_00046game_00046GdxDefinition__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
