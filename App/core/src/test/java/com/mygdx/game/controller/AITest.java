d150ebef89da12086884e4d35f17e1324bb507bd
Unknown <jonas_malheiro_sousa@hotmail.com>
2018-06-03 22:51:16 +0100

    Tests to the board added

--- App/core/src/test/java/com/mygdx/game/controller/AITest.java
+++ App/core/src/test/java/com/mygdx/game/controller/AITest.java
@@ -1,5 +1,10 @@
 package com.mygdx.game.controller;
 
+import com.mygdx.game.model.Board;
+import com.mygdx.game.model.Cruiser;
+import com.mygdx.game.model.Ship;
+import com.mygdx.game.model.Submarine;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -133,5 +138,98 @@ public class AITest {
 
         assertTrue(hBoard.check());
         assertFalse(cBoard.check());
+    }
+
+
+    @Test
+    public void overPutShips() {
+
+        BoardController boardC = new BoardController();
+
+        Board board = new Board(10,boardC);
+        Ship submarine = new Submarine();
+        ShipController shipC = new ShipController(submarine);
+
+        Ship cruiser = new Cruiser();
+        ShipController shipC2 = new ShipController(cruiser);
+
+
+        assertFalse (shipC.isPlaced());
+        assertFalse (shipC2.isPlaced());
+
+        shipC.update(board, 5,5);
+
+        assertTrue(shipC.isPlaced());
+
+        shipC2.update(board, 5,5);
+
+        assertFalse (shipC2.isPlaced());
+
+    }
+
+    @Test
+    public void rotateAndPutShip() {
+        //Tests if it possible to rotate and put the ship in the board
+
+        BoardController boardC = new BoardController();
+
+        Board board = new Board(10,boardC);
+        Ship submarine = new Submarine();
+        ShipController shipC = new ShipController(submarine);
+
+        assertFalse (shipC.isPlaced());
+
+        shipC.rotate();
+        shipC.update(board, 5,5);
+
+        assertTrue(shipC.isPlaced());
+        assertEquals(board.getMatrix()[5][5].getCellModel().getShip(),shipC);
+        assertEquals(board.getMatrix()[6][5].getCellModel().getShip(),shipC);
+        assertEquals(board.getMatrix()[7][5].getCellModel().getShip(),shipC);
+    }
+
+    @Test
+    public void shootShipDown() {
+
+        BoardController boardC = new BoardController();
+
+        Board board = new Board(10, boardC);
+        Ship submarine = new Submarine();
+        ShipController shipC = new ShipController(submarine);
+
+        shipC.update(board, 5,5);
+        assertTrue(shipC.isPlaced());
+
+        board.getMatrix()[5][5].destroy(false);
+        board.getMatrix()[5][6].destroy(false);
+        board.getMatrix()[5][7].destroy(false);
+
+        assertTrue(submarine.check());
+
+    }
+
+    @Test
+    public void failInsertionBecauseDirectionAndIndex() {
+
+        BoardController boardC = new BoardController();
+
+        Board board = new Board(10, boardC);
+        Ship submarine = new Submarine();
+        ShipController shipC = new ShipController(submarine);
+
+        assertFalse (shipC.isPlaced());
+
+        shipC.rotate();
+        shipC.rotate();
+        shipC.rotate();
+        shipC.update(board,1,1);
+
+        assertFalse (shipC.isPlaced());
+
+        shipC.update(board,11,11);
+
+        assertFalse (shipC.isPlaced());
+
+
     }
 }
