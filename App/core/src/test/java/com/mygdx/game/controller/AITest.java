edaa84edb39e34b15a8ab34930fd6838f68bffe2
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-06-03 22:37:04 +0100

    improved AI tests

--- App/core/src/test/java/com/mygdx/game/controller/AITest.java
+++ App/core/src/test/java/com/mygdx/game/controller/AITest.java
@@ -17,6 +17,9 @@ public class AITest {
         int cells = BoardController.getDimension() * BoardController.getDimension();
         for (int i = 0; i < cells; i++){
             behaviour.shoot(board);
+
+            if(board.check())
+                break;
         }
 
         assertTrue(board.check());
@@ -30,9 +33,12 @@ public class AITest {
 
         board.populate();
 
-        int cells = 75;
+        int cells = BoardController.getDimension() * BoardController.getDimension();
         for (int i = 0; i < cells; i++){
             behaviour.shoot(board);
+
+            if(board.check())
+                break;
         }
 
         assertTrue(board.check());
@@ -46,11 +52,38 @@ public class AITest {
 
         board.populate();
 
-        int cells = 75;
+        int cells = BoardController.getDimension() * BoardController.getDimension();
         for (int i = 0; i < cells; i++){
             behaviour.shoot(board);
+
+            if(board.check())
+                break;
         }
 
         assertTrue(board.check());
+    }
+
+    @Test(timeout=5000)
+    public void HardVSEasy() {
+        HardBehaviour hBehaviour = new HardBehaviour();
+        EasyBehaviour eBehaviour = new EasyBehaviour();
+
+        BoardController eBoard= new BoardController();
+        BoardController hBoard= new BoardController();
+
+        eBoard.populate();
+        hBoard.populate();
+
+        int cells = BoardController.getDimension() * BoardController.getDimension();
+        for (int i = 0; i < cells; i++){
+            hBehaviour.shoot(eBoard);
+            eBehaviour.shoot(hBoard);
+
+            if(hBoard.check() || eBoard.check())
+                break;
+        }
+
+        assertTrue(eBoard.check());
+        assertFalse(hBoard.check());
     }
 }
